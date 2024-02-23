95c0433911af91470080124c184694901f41486a
Administrator <abhay.shaw1@sunknowledge.com>
2024-02-23 02:39:18 -0500

    1st commit

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,52 @@
+import React from 'react';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+/**import all components  */
+import Username from './components/Username';
+import Password from './components/Password';
+import Reset from './components/Reset';
+import Register from './components/Register';
+import Recovery from './components/Recovery';
+import Profile from './components/Profile';
+import PageNotFound from './components/PageNotFound';
+
+/** root routes  */
+const router = createBrowserRouter([
+  {
+    path : '/',
+    element : <Username></Username>
+  },
+  {
+    path : '/register',
+    element : <Register></Register>
+  },
+  {
+    path : '/password',
+    element : <Password></Password>
+  },
+  {
+    path : '/reset',
+    element : <Reset></Reset>
+  },
+  {
+    path : '/recovery',
+    element : <Recovery></Recovery>
+  },
+  {
+    path : '/profile',
+    element : <Profile></Profile>
+  },
+  {
+    path : '/*',
+    element : <PageNotFound></PageNotFound>
+  },
+])
 
 function App() {
   return (
-    <div className="App">
-       <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
-    </div>
+    <main>
+      <RouterProvider router={router}></RouterProvider>
+    </main>
   );
 }
 
---
+++ client/src/components/Username.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import {Link} from 'react-router-dom'
+import avatar from '../assets/profile.png'
+
+import styles from '../styles/Username.module.css'
+
+export default function Username(){
+    return (
+       <div className='container mx-auto'>
+        <div className='flex  h-screen'>
+            <div className={styles.glass}>
+                <div className="title flex flex-col items-center">
+                    <h4 className='text-5xl font-bold'>Hello Again</h4>
+                    <span className="py-4 text-xl w-2/3 text-center text-gray-500">
+                        Explore More By Connecting with us.
+                    </span>
+                </div>
+                <form className="py-1">
+                    <div className="profile flex justify-center py-4">
+                        <img src={avatar} alt="avatar" />
+                    </div>
+                    <div className="textbox">
+                        <input type="text" placeholder='Username' />
+                        <button type='submit'>Let's Go</button>
+                    </div>
+
+                    <div className="text-center py-4">
+                        <span className='text-grey-500'>
+                            Not a Member 
+                            <u>
+                                <Link className='text-red-500' to="/register">Register Now</Link>
+                            </u>
+                        </span>
+                    </div>
+                </form>
+            </div>
+        </div>
+       </div>  
+    )
+}   
